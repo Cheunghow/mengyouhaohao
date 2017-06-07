795b2beddb1d0a11fc36767d37dc23ae197ff51a
Cheunghow <Cheunghow@126.com>
2017-06-07 11:06:11 +0800

    deleted test1

--- find_bigfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat <<Menu
-
->>>>>>>>>>>>>>>>>>>>CHECK BIG DIRECTORY and FILE<<<<<<<<<<<<<<<<<<<<<<< 
-
-Menu
-
-du -ag|sort -rn|awk '{if ($1>=5) print}'|sort -k 2|awk '{if (NR>2) print}'   
